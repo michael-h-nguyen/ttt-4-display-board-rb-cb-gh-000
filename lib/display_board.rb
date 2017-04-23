8f3dc5e4d71e5ff97c0e6a994f911dbe9241a6bb
Michael Nguyen <mikemhn@gmail.com>
2017-04-23 16:28:04 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(ticktack)
+  puts " #{ticktack[0]} | #{ticktack[1]} | #{ticktack[2]} "
+  puts "-----------"
+  puts " #{ticktack[3]} | #{ticktack[4]} | #{ticktack[5]} "
+  puts "-----------"
+  puts " #{ticktack[6]} | #{ticktack[7]} | #{ticktack[8]} "
+end
